d7c7aac4fd6939d885ea6a83506716b0b6bd1cbc
TcgVanguardTroll <jordangrant46@gmail.com>
2023-01-25 05:12:14 -0500

    Updating visit num function

--- main/alaya/alaya.go
+++ main/alaya/alaya.go
@@ -14,5 +14,29 @@ type Alaya struct {
 }
 
 func (a *Alaya) visitNum(ast alaya_ast.AST) int {
-	return
+    switch n := ast.(type) {
+    case *alaya_ast.Num:
+        return n.Value
+    case *alaya_ast.BinOp:
+        switch n.Op.Type {
+        case alaya_parser.PLUS:
+            return a.visitNum(n.Left) + a.visitNum(n.Right)
+        case alaya_parser.MINUS:
+            return a.visitNum(n.Left) - a.visitNum(n.Right)
+        case alaya_parser.MUL:
+            return a.visitNum(n.Left) * a.visitNum(n.Right)
+        case alaya_parser.DIV:
+            return a.visitNum(n.Left) / a.visitNum(n.Right)
+        }
+    case *alaya_ast.UnaryOp:
+        switch n.Op.Type {
+        case alaya_parser.PLUS:
+            return a.visitNum(n.Expr)
+        case alaya_parser.MINUS:
+            return -1 * a.visitNum(n.Expr)
+        }
+    default:
+        // Handle other types of nodes or raise an error.
+        return -1
+    }
 }
